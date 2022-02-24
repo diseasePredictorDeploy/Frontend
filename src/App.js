41c1560d52971f0fc30ba17384f4b21dbadbce50
Sitanshu Pokalwar <sitz2309@gmail.com>
2022-02-24 13:11:36 +0530

    text

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-center">
-      Under Construction 🤞🤞😁😁❤😉
-    </h1>
-  )
+    <h1 className="text-3xl font-bold underline text-center">Under Progress</h1>
+  );
 }
