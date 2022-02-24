8298155ecfee58d0c5dc238ab5b0bd235a0e18db
Udit-Gogia <gogiaudit17@gmail.com>
2022-02-24 18:16:08 +0530

    SignUp

--- src/components/signIn.jsx
+++ src/components/signIn.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Email from "../assets/images/email.svg"
+import Google from "../assets/images/google.svg"
 // import lottie from "lottie-web";
 // import Doctor from "../assets/images/doclottie.json";
 
@@ -12,18 +14,49 @@ import React from "react";
 //     });
 //   }, []);
 
-const SignIn = () => {
+
+
+export default function(){
+
+  let options = [
+    {
+    icon: Email,
+    name : "Email",
+    dim : "w-7",
+    width: "w-[125px]"
+    },
+    {
+    icon: Google,
+    name : "Google",
+    dim : "w-7",
+    width: "w-[125px]",
+    }
+  ]
+
+  
+
+  
+  
   return (
-    <section className="grid place-content-center min-h-screen min-w-full">
+    <section className="grid place-content-center min-h-screen min-w-full bg-[rgb(254,248,237)]">
       <div className="flex">
-        <div className="flex flex-col">
-          <div className="">
-            <h1>Create Account.</h1>
-            <p>Share your thoughts with the world from today.</p>
+        <div className="flex flex-col justify-center ">{/*ADD SPACE-y-20 HERE*/}
+          <div className="space-y-3">
+            <h1 className="text-[#30194F] font-bold text-4xl ">Create Account.</h1>
+            <p className="text-[#BDAAC2]">Share your thoughts with the world from today.</p>
           </div>
           <div className="">
             Continue with ...
-            <div className="">{/* card */}</div>
+            <div className="">
+              <div className="flex gap-4">
+                {options.map((props)=>
+                    <div className = {`border-2 border-[#BDAAC2] hover:border-[#6A316A] flex px-8 py-5  flex-col items-center gap-3 ${props.width} rounded-sm`}>
+                      <img src={props.icon} className={props.dim}></img>
+                      <p>{props.name}</p>
+                    </div>
+                )}
+              </div>
+            </div>
           </div>
           <div className="">
             <p>Already have an account</p>
@@ -44,4 +77,3 @@ const SignIn = () => {
   );
 };
 
-export default SignIn;
