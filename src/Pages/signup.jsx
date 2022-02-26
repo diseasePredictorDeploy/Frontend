dc7442c935cd94337ce07018775223dae890e489
Udit-Gogia <gogiaudit17@gmail.com>
2022-02-26 12:10:42 +0530

    AboutCardCreated

--- src/Pages/signup.jsx
+++ src/Pages/signup.jsx
@@ -6,32 +6,32 @@ import SignUpContent from "../components/SignUpContent";
 import SignInContent from "../components/SignInContent";
 
 export default function Signup() {
-  React.useEffect(() => {
-    lottie.loadAnimation({
-      container: document.querySelector("#docLottie"),
-      animationData: Doctor,
-      loop: true, // boolean
-      autoplay: true, // boolean
-    });
-  }, []);
+	React.useEffect(() => {
+		lottie.loadAnimation({
+			container: document.querySelector("#docLottie"),
+			animationData: Doctor,
+			loop: true, // boolean
+			autoplay: true, // boolean
+		});
+	}, []);
 
-  return (
-    <Router>
-      <section className="box-border grid place-content-center min-h-screen min-w-full bg-[#FEF8ED]">
-        <div className="container flex w-screen">
-          <Switch>
-            <Route path="/">
-              <SignUpContent />
-            </Route>
-            <Route path="/signin">
-              <SignInContent />
-            </Route>
-          </Switch>
-          <div className="basis-[50%] overflow-x-visible">
-            <div id="docLottie" style={{ width: 700, height: 700 }}></div>
-          </div>
-        </div>
-      </section>
-    </Router>
-  );
+	return (
+		<Router>
+			<section className="box-border grid place-content-center min-h-screen min-w-full bg-[#FEF8ED]">
+				<div className="container flex w-screen">
+					<Switch>
+						<Route path="/">
+							<SignUpContent />
+						</Route>
+						<Route path="/signin">
+							<SignInContent />
+						</Route>
+					</Switch>
+					<div className="basis-[50%] overflow-x-visible">
+						<div id="docLottie" style={{ width: 700, height: 700 }}></div>
+					</div>
+				</div>
+			</section>
+		</Router>
+	);
 }
