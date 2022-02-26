f6e1a2289ac80c55537c6e41c357765b5fb63f42
Sanjeeban2411 <sanju.sanjeeban.sp@gmail.com>
2022-02-26 22:06:41 +0530

    React-Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+} from "react-router-dom";
+
 import Signup from './Pages/signup'
-// import About from './Pages/about'
+import About from './Pages/about'
+import Home from './Pages/home'
 
 function App() {
   return (
-    <div>
-      <Signup />
-    </div>
+    <Router>
+      <div>
+      <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
---
+++ src/Pages/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function home() {
+  return (
+    <div>
+        <a href="/signup">Signup Page</a>
+        <br/>
+        <a href="/about">Aboutus Page</a>
+    </div>
+  )
+}
