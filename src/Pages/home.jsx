d757f63b4b64cd02afb891737a362688c56b60c2
Udit-Gogia <gogiaudit17@gmail.com>
2022-02-28 22:13:46 +0530

    TypeWritter

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import lottie from 'lottie-web'
 import Navbar from '../components/Navbar'
 // import Wave from '../assets/images/wave.gif'
+import Landing from '../assets/images/Landing.json'
 
-export default function home() {
+import Typewriter from 'typewriter-effect/dist/core'
+
+export default function Home() {
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: document.querySelector('#homeLottie'),
+      animationData: Landing,
+      loop: true, // boolean
+      autoplay: true, // boolean
+    })
+  }, [])
+
+  new Typewriter('#typewriter', {
+    strings: ['Hello', 'World'],
+    autoStart: true,
+  })
+
   return (
-    <div className='bg-[#FEF8ED] h-screen'>
-        <Navbar/>
-      {/* <div>
-        <img className='w-screen absolute -bottom-0' src={Wave} alt="" />
-      </div> */}
+    <div className="bg-[#FEF8ED] h-screen overflow-x-hidden">
+      <Navbar />
+      <div className="flex flex-row">
+        {/* <div className="" id="typewriter">
+          abc
+        </div> */}
+
+        <div className=" overflow-hidden">
+          <div
+            id="homeLottie"
+            style={{ width: 740, height: 740 }}
+            className="px-auto py-auto"
+          ></div>
+        </div>
+      </div>
     </div>
   )
 }
