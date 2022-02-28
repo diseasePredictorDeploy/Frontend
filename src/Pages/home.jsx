ec1961926ee8a5fab6106c7d789400a1ffb9827c
Sanjeeban2411 <sanju.sanjeeban.sp@gmail.com>
2022-03-01 00:09:18 +0530

    minor changes w/ udit

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -22,21 +22,28 @@ export default function Home() {
   // })
 
   return (
-    <div className="bg-[#FEF8ED] h-screen overflow-x-hidden">
+    <div className="bg-[#FEF8ED] h-screen overflow-x-hidden px-6">
       <Navbar />
-      <div className="flex flex-row w-screen">
-        <Typewriter
-          onInit={(typewriter) => {
-            typewriter
-              .typeString('You have the symptoms,... We have the solution')
-              .start()
-          }}
-        />
-
-        <div className=" overflow-hidden">
+      <div className="flex flex-row w-screen ">
+        <div className="w-[50vw] text-4xl">
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString('You have the symptoms,... We have the solution')
+                .start()
+            }}
+          />
+          <div className="px-20 py-10 rounded-3xl border-2 border-[#C996CC] w-[70%] text-[20px] mt-32 mx-auto">
+          Virtual Vitals provide an easy way to assess your body. Simply enter in your symptoms and we will will find out whats wrong.
+          </div>
+          <div className='w-44 h-20 mx-auto bg-[#30194F] rounded-2xl mt-20 text-[#C996CC]'>
+            <button>Diagnose</button>
+          </div>
+        </div>
+        <div className=" overflow-hidden w-[50vw] ">
           <div
             id="homeLottie"
-            style={{ width: 740, height: 740 }}
+            style={{ width: 740, height: 610 }}
             className="px-auto py-auto"
           ></div>
         </div>
